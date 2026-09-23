53361f79031716df2bc66d24176cf24deaef7daf
louie5277 <33411320+louie5277@users.noreply.github.com>
2026-09-23 16:29:24 -0500

    Fix formatting of WSAIoctl call for keepalive

--- src/obs-vnc-source-thread.c
+++ src/obs-vnc-source-thread.c
@@ -252,7 +252,8 @@ static void enable_tcp_keepalive(rfbClient *client)
 	keepalive.keepaliveinterval = 5000;
 
 	DWORD bytes_returned = 0;
-	if (WSAIoctl(sock, SIO_KEEPALIVE_VALS, &keepalive, sizeof(keepalive), NULL, 0, &bytes_returned, NULL, NULL) != 0) {
+	if (WSAIoctl(sock, SIO_KEEPALIVE_VALS, &keepalive, sizeof(keepalive), NULL, 0, &bytes_returned,
+		     NULL, NULL) != 0) {
 		blog(LOG_WARNING, "obs-vnc: failed to configure TCP keepalive (WSA error %d)", WSAGetLastError());
 	}
 }
